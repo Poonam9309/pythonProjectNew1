ac6feaf52a9bb83ee970070ed4197f359833d861
Poonam8888 <poonamdhage0916@gmail.com>
2024-10-28 18:14:09 +0530

    13 Aug IDs are added

--- python4x/13thAug/ID.py
+++ python4x/13thAug/ID.py
@@ -3,7 +3,13 @@
 # print(id(age1))
 # print(id(age2))
 
-first_name = "poonam"
-last_name = "Dhage"
-full_name = first_name+("_")+last_name
-print(full_name)
+# first_name = "poonam"
+# last_name = "Dhage"
+# full_name = first_name+("_")+last_name
+# print(full_name)
+
+# table = 7
+# print(f"{table}*1={table*1}")
+# print(f"{table}*2={table*2}")
+# print(f"{table}*3={table*3}")
+# print(f"{table}*4={table*4}")
