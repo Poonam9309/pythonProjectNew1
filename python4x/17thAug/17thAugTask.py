1d5d22e9806cccb35cab80bf91c2421cc22b71af
Poonam8888 <poonamdhage0916@gmail.com>
2024-10-30 01:10:42 +0530

    17 Aug Task is added

---
+++ python4x/17thAug/17thAugTask.py
@@ -0,0 +1,39 @@
+# 1. Difference between = and == operators in Python
+# The = operator is used for assigning values to variables. e.g: x = 5 assigns the value 5 to the variable x.
+# The == operator is used for comparing if two values are equal. e.g: x == 5 checks if x is equal to 5 and returns True or False.
+
+# 2. What does the ** operator do in Python ?
+# The ** operator is used for exponentiation in Python. It raises the number on the left to
+# 2 ** 3 evaluates to 8 (2 raised to the power of 3)
+# x ** 2 squares the value of x.
+
+# 3. What does the ^ operator do in Python ?
+# The ^ operator in Python performs a bitwise XOR operation on operands. It compares each
+# The ^ operator is commonly used in contexts like:
+# Cryptography : for encrypting and decrypting data using XOR operations.
+
+# 4.Write a Python program to calculate the
+# pi = 3.14
+# radius = float(input("enter the radius of the circle: "))
+# area = pi * radius ** 2
+# print(f"the area of the circle with radius{radius} is {area:.2f}square units.")
+
+# 5. Create a program that takes two numbers as input and prints whether the first number is greater than, less than, or equal to second number.
+# num1 = float(input("enter the first number: "))
+# num2 = float(input("enter the second number: "))
+
+# if num1 > num2:
+#     print(f"{num1}is greater than {num2}")
+# elif num1 < num2:
+#     print(f"{num1}is less than {num2}")
+# else:
+#     print(f"{num1}is equal to {num2}")
+
+# 6. Develop a Python script that calculate the square and cube of given number
+# number = float(input("enter a number: "))
+
+# square = number ** 2
+# cube = number ** 3
+
+# print(f"The square of {number} is {square}")
+# print(f"The cube of {number} is {cube}")
